2a4eea86e679000f7212a76c8356413a3507532e
Dan Riding <doonyby@gmail.com>
2016-12-02 20:38:57 -0700

    server side validation

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classnames from "classnames";
 
 class SignupForm extends React.Component {
 	constructor(props) {
@@ -7,7 +8,9 @@ class SignupForm extends React.Component {
 			username: '',
 			email: '',
 			password: '',
-			passwordConfirmation: ''
+			passwordConfirmation: '',
+			errors: {},
+			isLoading: false
 		}
 		this.onChange = this.onChange.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
@@ -18,15 +21,20 @@ class SignupForm extends React.Component {
 	}
 
 	onSubmit(e) {
+		this.setState({ errors: {}, isLoading: true });
 		e.preventDefault();
-		this.props.userSignupRequest(this.state);
+		this.props.userSignupRequest(this.state).then(
+			() => {},
+			(err) => this.setState({ errors: err.response.data, isLoading: false })
+		);
 	}
 
 	render() {
+		const { errors } = this.state;
 		return (
 			<form onSubmit={this.onSubmit} >
 				<h1>Join our community!</h1>
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.username })}>
 					<label className="control-label">Username</label>
 					<input 
 						value={this.state.username} 
@@ -34,17 +42,19 @@ class SignupForm extends React.Component {
 						type="text" 
 						name="username" 
 						className="form-control" />
+						{errors.username && <span className="help-block">{errors.username}</span>}
 				</div>
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.email })}>
 					<label className="control-label">Email</label>
 					<input 
 						value={this.state.email} 
 						onChange={this.onChange} 
-						type="email" 
+						type="text" 
 						name="email" 
 						className="form-control" />
+						{errors.email && <span className="help-block">{errors.email}</span>}
 				</div>
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.password })}>
 					<label className="control-label">Password</label>
 					<input 
 						value={this.state.password} 
@@ -52,8 +62,9 @@ class SignupForm extends React.Component {
 						type="password" 
 						name="password" 
 						className="form-control" />
+						{errors.password && <span className="help-block">{errors.password}</span>}
 				</div>
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.passwordConfirmation })}>
 					<label className="control-label">PasswordConfirmation</label>
 					<input 
 						value={this.state.passwordConfirmation} 
@@ -61,9 +72,10 @@ class SignupForm extends React.Component {
 						type="password" 
 						name="passwordConfirmation" 
 						className="form-control" />
+						{errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
 				</div>				
 				<div className="form-group">
-					<button className="btn btn-primary btn-large">Sign up</button>
+					<button disabled={this.state.isLoading} className="btn btn-primary btn-large">Sign up</button>
 				</div>
 			</form>
 		);
